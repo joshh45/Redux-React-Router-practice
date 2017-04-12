ba6eb1b2eb5f2200739fadfd9c7ae900f651f801
Josh Ho <hojo1025@gmail.com>
2017-04-11 21:04:06 -0400

    route imports

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -5,7 +5,7 @@ import { routerReducer} from 'react-router-redux';
 import posts from './posts';
 import comments from './comments';
 
-const rootReducer = combineReducers({posts, comments, router: routerReducer});
+const rootReducer = combineReducers({posts, comments, routing: routerReducer});
 
 
 export default rootReducer;
